c557a0b05cc0551853eebac91fabad77a12d9984
riwuks <riwuko@gmail.com>
2018-01-28 11:39:50 +0100

    Paulina

--- funkcjaPepe.cpp
+++ funkcjaPepe.cpp
@@ -6,6 +6,6 @@ cout <<"Ale co ona robi?"<<endl;
 cout <<"Podaj jakas liczbe calkowita"<<endl;
 cin >> x;
 cout <<"Podales nastepujaca liczbe:" << x <<endl;
-
+cout <<"Gratulacje!"<<endl;
 return 0;
 }
